3662c4ad414a10ee3986cfb613717ab10a5bbf7c
Tyler G Alexander <tylergeorgealexander@gmail.com>
2021-11-10 14:32:17 -0800

    pages still rendering properly, added moar bootstrap

--- src/Components/Cards/AddCard.js
+++ src/Components/Cards/AddCard.js
@@ -1,10 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { readDeck, listDecks } from "../../utils/api";
 
-export default function AddCard(){
+export default function AddCard() {
+  const [deck, setDeck] = useState([{name:"ph", description:"ph"}]);
+  const [deckName, setDeckName] = useState("");
+  const [deckDesc, setDeckDesc] = useState("");
+  const { deckId } = useParams();
 
-    return (
-        <div>
-            "Add Card"
-        </div>
-    )
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await readDeck(deckId);
+      setDeck(response);
+    }
+    fetchMyAPI();
     
+  }, [deckId]);
+
+console.log(deck)
+// setDeckName(deck[deckId].name)
+// setDeckDesc(deck[deckId].description)
+
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <Link to="/">
+              <span class="oi oi-home"></span> Home
+            </Link>
+          </li>
+          <li class="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Add Card
+          </li>
+        </ol>
+      </nav>
+      <h2>{deckName}{deck.name}: Add Card</h2>
+        <p class="mt-2">Front</p>
+      <form>
+        <textarea
+          type="textarea"
+          name="text"
+          id="text"
+          required
+          value="Front side of card ph"
+          onChange={(event) => setDeckName(event.target.value)}
+        />
+      </form>
+      <p class="mt-2">Back</p>
+      <form>
+        <textarea
+          type="textarea"
+          name="textarea"
+          id="textarea"
+          required
+          value="Back side of card ph"
+          onChange={(event) => setDeckName(event.target.value)}
+        />
+      </form>
+      <button class="btn btn-secondary mr-2 mt-2">Done</button>
+      <button class="btn btn-primary mt-2" type="submit">Save</button>
+    </div>
+  );
 }
--- src/Components/Decks/EditDeck.js
+++ src/Components/Decks/EditDeck.js
@@ -1,28 +1,76 @@
 import React, { useState, useEffect } from "react";
-import { readDeck } from "../../utils/api"
+import { Link, useParams } from "react-router-dom";
+import { readDeck, listDecks } from "../../utils/api";
 
-export default function EditDecks(){
-    // constructing a deck using useState
-    const [deck, setDeck] = useState({});
-    // example of a variable to set the form input value equal to
-    const [deckName, setDeckName] = useState("");
-    // deckId could be the name of the deck we need to read
-    const deckId = "This would probably be an object/array passed with Edit Decks props"
+export default function EditDecks() {
+  // constructing a deck using useState
+  const [deck, setDeck] = useState([{ name: "ph", description: "ph" }]);
+  // example of a variable to set the form input value equal to
+  const [deckName, setDeckName] = useState("");
+  const [deckDesc, setDeckDesc] = useState("");
+  // deckId could be the name of the deck we need to read
+  const { deckId } = useParams();
 
-    useEffect(() => {
-        readDeck(deckId).then(data => setDeck(deck));
-    }, []);
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await readDeck(deckId);
+      setDeck(response);
+    }
+    fetchMyAPI();
+  }, [deckId]);
 
+  console.log(deck);
+  // setDeckName(deck[deckId].name)
+  // setDeckDesc(deck[deckId].description)
 
-    // breadcrumbs is the greyed out navigation box in the app, look into bootstrap for more references
-    return (
-        <div>
-            <div className="breadcrumbs"></div>
-            <header></header>
-            {/* <Link to={"/"}>Something</Link> */}
-            <form>
-                <input type="" name="" id="" value={deckName} onChange={(event => setDeckName(event.target.value))} />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <Link to="/">
+              <span class="oi oi-home"></span> Home
+            </Link>
+          </li>
+          <li class="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Edit Deck
+          </li>
+        </ol>
+      </nav>
+      <h2>Edit Deck</h2>
+      <form>
+        <label class="mt-2">Name</label>
+
+        <textarea
+          type="text"
+          name="text"
+          id="text"
+          style={{ width: "100%" }}
+          required
+          value={deck.name}
+          onChange={(event) => setDeckName(event.target.value)}
+        />
+
+        <label class="mt-2">Description</label>
+
+        <textarea
+          type="text"
+          name="text"
+          id="text"
+          style={{ width: "100%" }}
+          required
+          value={deck.description}
+          onChange={(event) => setDeckName(event.target.value)}
+        />
+
+        <button class="btn btn-secondary mr-2 mt-2">Cancel</button>
+        <button class="btn btn-primary mt-2" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
